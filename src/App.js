9eaf769625b9c69c0035642191bf92713f8cd1d2
rahulranve8925 <rahulranve8925@gmail.com>
2025-04-23 01:33:16 +0530

    login-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to first react app</h1>
+      <Login/>
     </div>
   );
 }
